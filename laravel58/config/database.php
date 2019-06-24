1c32c25777d733696069d9b4301469c73ff3424a
zhangyu <zhangyu@xiongying.com>
2019-06-24 15:26:13 +0800

    qqqq

--- laravel58/config/database.php
+++ laravel58/config/database.php
@@ -129,7 +129,7 @@ return [
         'default' => [
 	        'host' => env('REDIS_HOST', '127.0.0.1'),
 	        'password' => env('REDIS_PASSWORD', null),
-	        'port' => env('REDIS_PORT', 6479),
+	        'port' => env('REDIS_PORT', 6379),
 	        'database' => 0,
 	        'read_write_timeout' => 0,//new
         ],
